845697d8be3e76b50bd7da8bc1722acdf9ee5634
VrenTati <kateryna.vynokurova.pz.2022@lpnu.ua>
2025-01-11 12:57:39 +0200

    Add alembic

---
+++ app/core/config.py
@@ -0,0 +1,69 @@
+from typing import Dict, List
+
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class RunConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 8000
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    user: str = "/user"
+    auth: str = "/auth"
+    categories: str = "/categories"
+    transactions: str = "/transactions"
+    reports: str = "/reports"
+    user_categories: str = "/user_categories"
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+    @property
+    def bearer_token_url(self) -> str:
+        parts = (self.prefix, self.v1.prefix, self.v1.auth, "/login")
+        path = "".join(parts)
+        return path.removeprefix("/")
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
+
+    @property
+    def convention(self) -> Dict[str, str]:
+        return {
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+    access_token: AccessToken
+
+
+settings = Settings()
